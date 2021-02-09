d696969bd2f2675dcbf9e134811799550a650939
Ahmed Saeed <eng.ahmeds3ed@gmail.com>
2021-02-09 03:22:53 +0200

    add is_day condition

--- src/Conditions/ServiceProvider.php
+++ src/Conditions/ServiceProvider.php
@@ -35,6 +35,7 @@ class ServiceProvider extends AbstractServiceProvider {
 		'condition_is_category',
 		'condition_is_tag',
 		'condition_is_date',
+		'condition_is_day',
 	];
 
 	/**
@@ -62,5 +63,6 @@ class ServiceProvider extends AbstractServiceProvider {
 		$container->share( 'condition_is_category', '\WP_Rules\Conditions\IsCategory' );
 		$container->share( 'condition_is_tag', '\WP_Rules\Conditions\IsTag' );
 		$container->share( 'condition_is_date', '\WP_Rules\Conditions\IsDate' );
+		$container->share( 'condition_is_day', '\WP_Rules\Conditions\IsDay' );
 	}
 }
